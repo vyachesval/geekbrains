43c108f48a37f7b9a534e10a8e3f7679d3279460
Gavrilov V <vyacheslav.gavrilov@domru.ru>
2019-08-13 15:46:07 +0300

    Практика 6. Сокеты. Сделана основа задания

---
+++ lessons/src/lesson6/practic6/client/Main.java
@@ -0,0 +1,47 @@
+package lesson6.practic6.client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        final String IP_ADRESS = "localhost";
+        final int PORT = 8189;
+
+        Socket socket;
+        DataInputStream in;
+        DataOutputStream out;
+
+        try {
+            socket = new Socket(IP_ADRESS, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = in.readUTF();
+                        System.out.println(str + "\n");
+                        Scanner sc = new Scanner(socket.getInputStream());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
