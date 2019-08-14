998a0c65737673f6931664a400dc123bf92a0704
Gavrilov V <viacheslav.gavrilov@domru.ru>
2019-08-14 21:18:30 +0300

    Урок 6. Работа с сетью

--- lessons/src/lesson6/practic6/client/Main.java
+++ lessons/src/lesson6/practic6/client/Main.java
@@ -1,47 +1,56 @@
 package lesson6.practic6.client;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
-        final String IP_ADRESS = "localhost";
+    public static void main(String[] args) {
+        final String IP_ADRESS = "127.0.0.1";
         final int PORT = 8189;
 
-        Socket socket;
+        Socket socket = null;
         DataInputStream in;
         DataOutputStream out;
 
         try {
             socket = new Socket(IP_ADRESS, PORT);
-            in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             new Thread(() -> {
                 try {
                     while (true) {
-                        String str = in.readUTF();
-                        System.out.println(str + "\n");
-                        Scanner sc = new Scanner(socket.getInputStream());
+                        while (!input.ready()) ;
+
+                        String str = input.readLine();
+                        System.out.println("From server: " + str.trim());
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
-                } finally {
-                    try {
-                        socket.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
                 }
             }).start();
 
+            Scanner scLoc = new Scanner(System.in);
+            while (true) {
+                try {
+                    String str = scLoc.nextLine();
+                    out.writeUTF(str + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                System.out.println("socket.close");
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
